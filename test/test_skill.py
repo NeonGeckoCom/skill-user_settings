7f40929a4e112efe5cffd3bf955e271dcaa52ec7
Daniel McKnight <daniel@neongecko.com>
2023-04-10 19:25:29 -0700

    Revert test approximate location checks

--- test/test_skill.py
+++ test/test_skill.py
@@ -438,8 +438,8 @@ class TestSkill(unittest.TestCase):
                              test_profile["location"][setting])
         self.assertEqual(profile["location"]["city"], "Honolulu")
         self.assertEqual(profile["location"]["state"], "Hawaii")
-        self.assertAlmostEqual(profile["location"]["lat"], 21.2890997, 4)
-        self.assertAlmostEqual(profile["location"]["lng"], -157.717299, 4)
+        self.assertAlmostEqual(profile["location"]["lat"], 21.2890997, 1)
+        self.assertAlmostEqual(profile["location"]["lng"], -157.717299, 1)
         self.assertEqual(profile["location"]["tz"], "Pacific/Honolulu")
         self.assertEqual(profile["location"]["utc"], -10.0)
 
